4fe8c0f36e620ff2f417ed074d3ed646a65ef15f
lsama8977-ctrl <lsama8977@gmail.com>
2026-09-13 22:44:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
-  "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  'HAPPY BIRTHDAY HBIBA MALAK',//untuk deskripsi planet layer 1
+  "I LOVE U MIMI",//untuk deskripsi planet layer 2
+  "♡Happy BIRTHDAY day HBIBA DYALI MALAK♡",//untuk deskripsi planet layer 3
+  "14/09/2008",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
